6123a1062d1072f62a1676c8389d2b1e54eace99
Kshitiz Saxena <kshitizsaxena1234@gmail.com>
2024-01-30 18:41:19 +0530

    Initial Commit

---
+++ QuizAppProject/src/main/java/com/horizon/quizapp/QuizAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.horizon.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppProjectApplication.class, args);
+	}
+
+}
